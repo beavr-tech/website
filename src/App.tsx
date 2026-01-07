0d37753b377a1794352b497be3b64eb86184d388
Priyanshu879 <priyanshuapps87@gmail.com>
2026-01-07 18:27:02 +0530

    add vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
+import { Analytics } from "@vercel/analytics/react"
+
+
 const queryClient = new QueryClient();
 
 const App = () => (
@@ -20,6 +23,10 @@ const App = () => (
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
+
+      {/* Vercel Analytics */}
+      <Analytics />
+
     </TooltipProvider>
   </QueryClientProvider>
 );
